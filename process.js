7fc66efab5c4941556ea3d0921bd2ddb327639e3
paulgiordano123 <22940569+paulgiordano123@users.noreply.github.com>
2020-01-06 16:59:07 -0500

    Add files via upload
    initial commit

---
+++ process.js
@@ -0,0 +1,65 @@
+// File Name: process.js
+// Student Name: Paul Giordano
+// Description: Project using web workers for image processing
+
+self.onmessage = function(data) {
+    // Start time
+    let myArr = []
+    let start = Date.now();
+    let medianData = median(data.data[0].data, data.data[0].width, data.data[0].height);
+    data.data[0].data.set(medianData);
+    // Calculate and add elapsed time
+    let elapsed = Date.now() - start;
+    data.data.push(elapsed);
+    self.postMessage(data.data);
+};
+
+// Apply the color median filter to the ImageData and replace data.
+// @param {Uint8ClampedArray} arr  array holding image data
+// @param {number}            w    Width of image to process
+// @param {number}            h    Height of image to process
+function median(arr, w, h) {
+    let r, c, rr, cc, i, j, k;
+
+    // Create output array
+    let arr2 = new Uint8ClampedArray( new ArrayBuffer(4*w*h) );
+
+    // Loop over all pixels
+    for (r=1; r<h-1; r++) {
+        for (c=1; c<w-1; c++) {
+
+            let ared = [];
+            let agrn = [];
+            let ablu = [];
+
+            for (i=-1; i<2; i++) {
+                rr = r + i;
+                for (j=-1; j<2; j++) {
+                    cc = c + j;
+
+                    // Push channel values to three color arrays
+                    k  = 4*(rr*w + cc);
+                    ared.push( arr[k] );
+                    agrn.push( arr[k+1] );
+                    ablu.push( arr[k+2] );
+                }
+            }
+
+            // Order pixel values
+            ared.sort();
+            agrn.sort();
+            ablu.sort();
+
+            // Reset all values to the median
+            k  = 4*(r*w + c);
+            arr2[k]   = ared[4];
+            arr2[k+1] = agrn[4];
+            arr2[k+2] = ablu[4];
+
+            // Set alpha to a fixed value of 255
+            arr2[k+3] = 255;
+        }
+    }
+
+    return arr2;
+}
